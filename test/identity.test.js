2e68c34536d5043cdb0f13d5fa9bf4e1293ac16b
SondreB <sondre@outlook.com>
2025-04-06 11:21:48 +0200

    fix: update resolver implementation to use nostr and add additional test cases

--- test/identity.test.js
+++ test/identity.test.js
@@ -1,12 +1,14 @@
 import test from 'ava';
-import is from '../lib/index.js';
+import nostr from '../lib/index.js';
 import { Resolver } from 'did-resolver';
 
 test('Create the resolver', async t => {
-  const isResolver = is.getResolver();
-  t.assert(isResolver != null);
+  const relays = ['wss://relay.damus.io', 'wss://relay.primal.net', 'wss://nos.lol'];
+  const nostrResolver = nostr.getResolver(relays);
+  // const nostrResolver = nostrResolver.getResolver();
+  t.assert(nostrResolver != null);
 
-  const resolver = new Resolver(isResolver);
+  const resolver = new Resolver(nostrResolver);
   t.assert(resolver != null);
   let didResolution = await resolver.resolve('did:is:0f254e55a2633d468e92aa7dd5a76c0c9101fab8e282c8c20b3fefde0d68f218');
   t.assert(didResolution.didResolutionMetadata.error == 'unsupportedDidMethod');
@@ -33,4 +35,22 @@ test('Create the resolver', async t => {
 
   didResolution = await resolver.resolve('PHRcZvY4z86XxXey1VykYosy3BecdTDnUi');
   t.assert(didResolution.didResolutionMetadata.error == 'invalidDid');
+});
+
+test('Verify relay without profile or relay list', async t => {
+  const relays = ['wot.nostr.sats4.life'];
+  const nostrResolver = nostr.getResolver(relays);
+  t.assert(nostrResolver != null);
+
+  const resolver = new Resolver(nostrResolver);
+  t.assert(resolver != null);
+
+  // SondreB pubkey
+  let didResolution = await resolver.resolve('did:nostr:17e2889fba01021d048a13fd0ba108ad31c38326295460c21e69c43fa8fbe515');
+  t.assert(didResolution.didDocument?.id == 'did:nostr:17e2889fba01021d048a13fd0ba108ad31c38326295460c21e69c43fa8fbe515');
+  t.assert(didResolution.didDocumentMetadata.deactivated == false);
+  t.assert(didResolution.didResolutionMetadata.contentType == 'application/did+ld+json');
+
+  t.assert(didResolution.didDocument.verificationMethod[0].publicKeyMultibase == 'z6DtPPzVD8nXDKTHG3x8cx8UpoVP6VSBsXaDhSWcoysUnkEY');
+  t.assert(didResolution.didDocument.service.length == 0);
 });
